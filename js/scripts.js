d42be817788877ad94995cba0e4b4ad0b04430ed
Chris Berry and Mike Gallegos <student@epicodus.com>
2016-01-13 11:19:15 -0800

    celeb matching site

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+  });
+//   var age = parseInt(prompt("How old are you?"));
+//
+//   if (age >= 18) {
+//     $('#adultcontent').show();
+//   } else {
+//     $('#under-18').show();
+//   }
+// });
+
+
+// $(document).ready(function() {
+//   var age = parseInt(prompt("What is your age?"));
+//
+// });
+// //   var animal = prompt("What is your favorite animal?");
+//
+//   if (animal === "turtle") {
+//     $('#turtle').show();
+//   } else if (animal === "bird") {
+//     $('#bird').show();
+//   }
+//     else if (animal === "snake") {
+//     $('#snake').show();
+//   }
+// else {
+// alert("Sorry this is currently not an option, please try again");
+// var animal = prompt("What is your favorite animal?");
+// }
+// });
