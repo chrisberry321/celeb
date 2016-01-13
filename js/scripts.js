5c328441fb373459e5c72fe9422b19bbddbfe140
Chris Berry and Mike Gallegos <student@epicodus.com>
2016-01-13 14:46:04 -0800

    protype branching logic working

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,48 @@
-$(document).ready(function() {
 
+
+$(document).ready(function() {
+  $('#blanks form').submit(function(event) {
+
+      age = parseInt($("input#age1").val());
+      orientation = $("select#orientation1").val();
+      gender = $("select#gender1").val();
+
+      // if (age >=50 && gender === "Female" && orientation === "Straight" || gender === "Male" && orientation === "Gay") {
+      //   $('#container-result1').show();
+      // }
+
+      if (age >= 50){
+        if (gender === "Female"){
+          if (orientation === "Straight") {
+            $('#container-result2').show();
+          }
+          else {
+            $('#container-result1').show();
+          }
+        }
+        else {}
+      }
+      else {}
+
+    event.preventDefault();
   });
+});
+
+  //     $('.name1').text(name1Input);
+  //     $('.address1').text(address1Input);
+  //     $('.city1').text(city1Input);
+  //     $('.state1').text(state1Input);
+  //     $('.zipcode1').text(zipcode1Input);
+  //
+  //     $('.story').show();
+  //
+  //     event.preventDefault();
+  //
+  //   });
+  // });
+
+
+
 //   var age = parseInt(prompt("How old are you?"));
 //
 //   if (age >= 18) {
